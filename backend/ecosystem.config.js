d5f0491f70d18b23f52960f3508f91ea1bd6842d
NataliyaNikulshina <victory515@yandex.ru>
2023-09-05 22:37:10 +0300

    Исправляет файл ecosystem

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -18,7 +18,7 @@ module.exports = {
       repo: DEPLOY_REPO,
       path: DEPLOY_PATH,
       'pre-deploy-local': `scp ./.env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}/current/backend`,
-      'post-deploy': `cd ${DEPLOY_PATH}/current/backend && npm i && npm run build`,
+      'post-deploy': `cd ${DEPLOY_PATH}/current/backend && npm i && npm run build && pm2 startOrRestart ecosystem.config.js `,
     },
   },
 };
