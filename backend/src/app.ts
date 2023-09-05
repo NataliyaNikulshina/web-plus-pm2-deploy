3b7b1e5c013d2981c3459307f228d9b8089e74d9
NataliyaNikulshina <victory515@yandex.ru>
2023-09-05 21:31:48 +0300

    fix

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -14,13 +14,7 @@ const app = express();
 mongoose.connect(DB_ADDRESS);
 
 // Только для локальных тестов. Не используйте это в продакшене
-app.use(cors({
-  origin: [
-    'https://mesto.nepysto.nomoredomainsicu.ru',
-    'https://api.mesto.nepysto.nomoredomainsicu.ru',
-  ],
-  optionsSuccessStatus: 200,
-}));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
